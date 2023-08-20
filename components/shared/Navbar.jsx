2ce958aa60ee8f05a781aa315f8764eec8244dc7
Musili Brian <musilibrian07@gmail.com>
2023-08-20 15:34:56 +0300

    navbar complete

--- components/shared/Navbar.jsx
+++ components/shared/Navbar.jsx
@@ -1,22 +1,26 @@
 "use client";
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { navLinks } from "@/constants";
 import Button from "./Button";
 import Link from "next/link";
+import Image from "next/image";
+import menu from "public/menu.svg";
+import close from "public/close.svg";
 
 const Navbar = () => {
-
-   const [toggle, setToggle] = useState(false);
+  const [toggle, setToggle] = useState(false);
 
   return (
     <section className="w-5/6 mx-auto">
       <nav className="flex items-center justify-between w-full py-6">
         {/* logo */}
-        <div>kk</div>
+        <div>
+          <p className="font-semibold tracking-wider text-[20px]">Musili</p>
+        </div>
 
         {/* links */}
         <div>
-          <ul className="items-center justify-end flex-1  hidden list-none sm:flex">
+          <ul className="items-center justify-end flex-1  hidden list-none md:flex">
             {navLinks.map((nav, i) => (
               <li
                 key={nav.id}
@@ -24,7 +28,7 @@ const Navbar = () => {
                   i === navLinks.length - 1 ? "mr-0" : "mr-10"
                 }`}
               >
-                <a href={`${nav.id}`}>{nav.title}</a>
+                <Link href={`${nav.id}`}>{nav.title}</Link>
               </li>
             ))}
           </ul>
@@ -42,9 +46,13 @@ const Navbar = () => {
         <div className="flex justify-end flex-1 md:hidden">
           {/* menu button */}
 
-          <div>menu</div>
+          <Image
+            src={toggle ? close : menu}
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain "
+            onClick={() => setToggle((prev) => !prev)}
+          />
 
-       
           {/* menu */}
           <div
             className={`${
@@ -53,15 +61,22 @@ const Navbar = () => {
           >
             <ul className="flex flex-col items-center justify-end flex-1 list-none">
               {navLinks.map((nav, i) => (
-                <li
-                  key={nav.id}
-                  className={`font-poppins font-normal cursor-pointer text-[16px] text-white  ${
-                    i === navLinks.length - 1 ? "mr-0" : "mb-4"
-                  }`}
-                >
-                  <a href={`#${nav.id}`}>{nav.title}</a>
-                </li>
+                <>
+                  <li
+                    key={nav.id}
+                    className={`font-poppins font-normal cursor-pointer text-[16px] text-white mb-4`}
+                    onClick={() => setToggle(false)}
+                  >
+                    <Link href={`${nav.id}`}>{nav.title}</Link>
+                  </li>
+                </>
               ))}
+              <li
+                onClick={() => setToggle(false)}
+                className="font-poppins font-normal cursor-pointer text-[16px] text-white "
+              >
+                <Link href={"/contact"}>Contact Me</Link>
+              </li>
             </ul>
           </div>
         </div>
